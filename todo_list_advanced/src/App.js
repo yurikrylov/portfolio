12ab7f10055ed7106153f2421e7426b0482fc357
Yuri Krylov <yuri.krylov@gmail.com>
2022-11-27 16:44:08 +0300

    20221127

--- todo_list_advanced/src/App.js
+++ todo_list_advanced/src/App.js
@@ -4,11 +4,23 @@ import { useState, useEffect } from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
 import Todo from './Components/Todo';
 import { db } from './firebase';
-import { query, collection, onSnapshot } from 'firebase/firestore'
+import { query, collection, onSnapshot, updateDoc, doc, addDoc, deleteDoc } from 'firebase/firestore'
 
 
 function App() {
   // create todo
+  const createToDo = async (e) => {
+    e.preventDefault(e)
+    if (input === '') {
+      alert('input some input')
+      return
+    }
+    await addDoc(collection(db, 'todos'), {
+      text: input,
+      completed: false
+    })
+    setInput('');
+  }
   // read todo from firebase 
   useEffect(() => {
     const q = query(collection(db, 'todos'))
@@ -19,19 +31,29 @@ function App() {
       })
       setTodos(todosArr)
     })
-    return ()=>unsubscribe()
+    return () => unsubscribe()
   }, [])
 
   // update todo 
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, 'todos', todo.id), {
+      completed: !todo.completed
+    })
+  }
+
   // delete todo
+  const deleteTodo = async (id)=>{
+await deleteDoc(doc(db, 'todos',id))
+  }
 
   const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
   return (
     <div className='bg'>
       <div className='container'>
         <h3 className='heading'>Todo App</h3>
-        <form action="">
-          <input className='inputtype="text' placeholder='Add todo' />
+        <form onSubmit={createToDo} action="" className='form'>
+          <input value={input} onChange={(e) => setInput(e.target.value)} className='inputtype="text' placeholder='Add todo' />
           <button className='button'><AiOutlinePlus size={30} /></button>
         </form>
         <ul>
@@ -39,8 +61,11 @@ function App() {
             <Todo
               key={index}
               todo={todo}
+              toggleComplete={toggleComplete}
+              deleteTodo ={deleteTodo}
             />))}
         </ul>
+        <p className='count'>{`you have ${todos.length} todo`} </p>
       </div>
     </div>
   );
--- todo_list_advanced/src/Components/Todo.jsx
+++ todo_list_advanced/src/Components/Todo.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {FaRegTrashAlt} from 'react-icons/fa';
 
-const Todo = ({todo}) => {
+const Todo = ({todo, toggleComplete, deleteTodo}) => {
   return (
-    <li className='li'>
+    <li className={todo.completed ? 'li': 'liComplited'}>
     <div className='row'>
-        <input type="checkbox" />
-        <p className='text'>{todo.text}</p>
+        <input onChange = {()=>toggleComplete(todo)} type="checkbox" checked = {todo.completed ?'checked':''}/>
+        <p onClick={()=>toggleComplete(todo)} className='text'>{todo.text}</p>
     </div>
-    <button>{<FaRegTrashAlt />}</button>
+    <button onClick={()=>{deleteTodo(todo.id)}} >{<FaRegTrashAlt />}</button>
     </li>
   )
 }
