b4da534c8de2fb8b3909dc488fc1abe4fe08333d
Yuri Krylov <yuri.krylov@gmail.com>
2022-12-13 07:40:33 +0300

    minor changes

--- todo_list_advanced/src/firebase.ts
+++ todo_list_advanced/src/firebase.ts
@@ -2,14 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from 'firebase/auth';
-const firebaseConfig = {
-  apiKey: "AIzaSyBbOjYQ117N-iQhKHSB2JOEYmlHZlnloS0",
-  authDomain: "todo-list-with-files.firebaseapp.com",
-  projectId: "todo-list-with-files",
-  storageBucket: "todo-list-with-files.appspot.com",
-  messagingSenderId: "602740564581",
-  appId: "1:602740564581:web:190c8ba1dde1b36813dc8c"
-};
+import {firebaseConfig} from './config/firebaseConfig';
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
