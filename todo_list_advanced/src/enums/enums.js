fe0fb1c6ad60912c2654bd5d87ff6de7b57d6dc6
Yuri Krylov <yuri.krylov@gmail.com>
2022-12-17 07:37:37 +0300

    typos

--- todo_list_advanced/src/enums/enums.js
+++ todo_list_advanced/src/enums/enums.js
@@ -1,11 +1,11 @@
 export const PROJECT_ACTIONS = {
     GET_ALL_PROJECTS:'get-all-projects',
-    ADD_PROJECT: 'add-todo',
-    TOGGLE_PROJECT: 'toggle-todo',
-    DELETE_PROJECT: 'delete-todo'
+    ADD_PROJECT: 'add-project',
+    TOGGLE_PROJECT: 'toggle-project',
+    DELETE_PROJECT: 'delete-project'
   }
   export const TASK_ACTIONS = {
-    ADD_TASK: 'add-todo',
-    TOGGLE_TASK: 'toggle-todo',
-    DELETE_TASK: 'delete-todo'
+    ADD_TASK: 'add-task',
+    TOGGLE_TASK: 'toggle-task',
+    DELETE_TASK: 'delete-task'
   }
